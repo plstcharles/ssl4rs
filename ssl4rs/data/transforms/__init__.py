73155b6661c9d024ee9af6ce35a9c510b6ab28b4
Pierre-Luc St-Charles <pierreluc.stcharles@gmail.com>
2023-04-08 21:36:10 -0400

    Add transform validation+identity impls

--- ssl4rs/data/transforms/__init__.py
+++ ssl4rs/data/transforms/__init__.py
@@ -1,5 +1,7 @@
 import typing
 
+import torchvision.transforms
+
 import ssl4rs.data.transforms.batch_sizer
 import ssl4rs.data.transforms.patchify
 import ssl4rs.data.transforms.tuple_mapper
@@ -8,4 +10,41 @@ from ssl4rs.data.transforms.patchify import Patchify
 from ssl4rs.data.transforms.tuple_mapper import TupleMapper
 
 BatchDictType = typing.Dict[typing.AnyStr, typing.Any]
-BatchTransformType = typing.Callable[[BatchDictType], BatchDictType]
+_BatchTransformType = typing.Callable[[BatchDictType], BatchDictType]
+BatchTransformType = typing.Union[_BatchTransformType, typing.Sequence[_BatchTransformType], None]
+
+
+class Identity:
+    """Simple/clean implementation of an identity transformation. Yep, it does nothing.
+
+    This may be useful for unit testing, in conditional transforms, or in composition operations.
+    """
+
+    def __call__(self, batch: typing.Any) -> typing.Any:
+        """Does nothing, and returns the provided batch object as-is.
+
+        Args:
+            batch: the batch object to be 'transformed'.
+
+        Returns:
+            The same batch object.
+        """
+        return batch
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}()"
+
+
+def validate_or_convert_transform(transform: BatchTransformType) -> BatchTransformType:
+    """Validates or converts the given transform object to a proper (torchvision-style) object."""
+    if transform is None:
+        return Identity()  # if nothing is provided, assume that's a shortcut for the identity func
+    if isinstance(transform, typing.Sequence):
+        assert all(
+            [callable(t) for t in transform]
+        ), "if a sequence of transforms is given, each transform must be a callable function"
+        if len(transform) == 0:
+            return Identity()  # there are no transforms to apply, return an identity function
+        return torchvision.transforms.Compose(transform)
+    assert callable(transform), "the batch transform function must be a callable object"
+    return transform
