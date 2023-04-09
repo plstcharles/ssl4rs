203a4aad63e0a69a735f6c9b7ab0c5c7c04c05eb
Pierre-Luc St-Charles <pierreluc.stcharles@gmail.com>
2023-04-08 21:35:48 -0400

    Add missing class imports to utils pkg init

--- ssl4rs/utils/__init__.py
+++ ssl4rs/utils/__init__.py
@@ -6,6 +6,7 @@ import ssl4rs.utils.imgproc
 import ssl4rs.utils.logging
 import ssl4rs.utils.patch_coord
 import ssl4rs.utils.stopwatch
+from ssl4rs.utils.filesystem import FileReaderProgressBar, WorkDirectoryContextManager
 from ssl4rs.utils.logging import get_logger
 from ssl4rs.utils.patch_coord import PatchCoord
 from ssl4rs.utils.stopwatch import Stopwatch
