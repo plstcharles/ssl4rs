f18b5401b34c1338ca37129b4cb74584d824cf53
Pierre-Luc St-Charles <pierreluc.stcharles@gmail.com>
2023-05-04 18:13:24 -0400

    Add dictconfig support to transforms init

--- ssl4rs/data/transforms/__init__.py
+++ ssl4rs/data/transforms/__init__.py
@@ -1,5 +1,7 @@
 import typing
 
+import hydra.utils
+import omegaconf
 import torchvision.transforms
 
 import ssl4rs.data.transforms.batch_sizer
@@ -39,12 +41,17 @@ def validate_or_convert_transform(transform: BatchTransformType) -> BatchTransfo
     """Validates or converts the given transform object to a proper (torchvision-style) object."""
     if transform is None:
         return Identity()  # if nothing is provided, assume that's a shortcut for the identity func
+    if isinstance(transform, (dict, omegaconf.DictConfig)) and "_target_" in transform:
+        transform = hydra.utils.instantiate(transform)
     if isinstance(transform, typing.Sequence):
-        assert all(
-            [callable(t) for t in transform]
-        ), "if a sequence of transforms is given, each transform must be a callable function"
-        if len(transform) == 0:
+        out_t = []
+        for t in transform:
+            if isinstance(t, (dict, omegaconf.DictConfig)) and "_target_" in t:
+                t = hydra.utils.instantiate(t)
+            assert callable(t), "if a sequence of transforms is given, each transform must be a callable function"
+            out_t.append(t)
+        if len(out_t) == 0:
             return Identity()  # there are no transforms to apply, return an identity function
-        return torchvision.transforms.Compose(transform)
+        return torchvision.transforms.Compose(out_t)
     assert callable(transform), "the batch transform function must be a callable object"
     return transform
