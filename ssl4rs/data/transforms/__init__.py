899e70063ac0c905da8b677c3efa90015ee3f006
Pierre-Luc St-Charles <pierreluc.stcharles@gmail.com>
2023-05-05 23:28:00 -0400

    Move ident transform to its own module

--- ssl4rs/data/transforms/__init__.py
+++ ssl4rs/data/transforms/__init__.py
@@ -5,36 +5,17 @@ import omegaconf
 import torchvision.transforms
 
 import ssl4rs.data.transforms.batch_sizer
+import ssl4rs.data.transforms.identity
 import ssl4rs.data.transforms.patchify
 import ssl4rs.data.transforms.tuple_mapper
 from ssl4rs.data.transforms.batch_sizer import BatchSizer, get_batch_size
+from ssl4rs.data.transforms.identity import Identity
 from ssl4rs.data.transforms.patchify import Patchify
 from ssl4rs.data.transforms.tuple_mapper import TupleMapper
 
 BatchDictType = typing.Dict[typing.AnyStr, typing.Any]
 _BatchTransformType = typing.Callable[[BatchDictType], BatchDictType]
 BatchTransformType = typing.Union[_BatchTransformType, typing.Sequence[_BatchTransformType], None]
-
-
-class Identity:
-    """Simple/clean implementation of an identity transformation. Yep, it does nothing.
-
-    This may be useful for unit testing, in conditional transforms, or in composition operations.
-    """
-
-    def __call__(self, batch: typing.Any) -> typing.Any:
-        """Does nothing, and returns the provided batch object as-is.
-
-        Args:
-            batch: the batch object to be 'transformed'.
-
-        Returns:
-            The same batch object.
-        """
-        return batch
-
-    def __repr__(self) -> str:
-        return f"{self.__class__.__name__}()"
 
 
 def validate_or_convert_transform(transform: BatchTransformType) -> BatchTransformType:
