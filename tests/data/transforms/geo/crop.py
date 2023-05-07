224b269ff6c20c4df5cf3464670f0a8309b482a7
Pierre-Luc St-Charles <pierreluc.stcharles@gmail.com>
2023-05-07 16:58:19 -0400

    Add geo crop interpolation test

--- tests/data/transforms/geo/crop.py
+++ tests/data/transforms/geo/crop.py
@@ -1,4 +1,6 @@
 import math
+import typing
+from unittest import mock
 
 import pytest
 import torch
@@ -67,3 +69,30 @@ def test_geo_crop_with_torchscript():
         out, new_gsd = t_compiled(data, gsd)
         assert out.shape == (1, 256, 256)
         assert 0.05 <= new_gsd <= 0.4
+
+
+def fake_resize(img: torch.Tensor, size: typing.Sequence[int], *args, **kwargs):
+    assert img.ndim == 3 and img.shape[0] == 1
+    assert (img == 1).all()
+    input_cols = img.shape[-1]
+    output_cols = size[-1]
+    output_val = input_cols / output_cols
+    return torch.ones(1, *size) * output_val
+
+
+@mock.patch("torchvision.transforms.functional.resize")
+def test_geo_crop_interpolation(resize_mock):
+    # note: this assumes that the base impl relies on torchvision's resize op
+    resize_mock.side_effect = fake_resize
+    data = torch.ones(1, 1024, 512)
+    gsd = 1.0
+    t = geo_crop.GroundSamplingDistanceAwareRandomResizedCrop(
+        size=(128, 256),
+        gsd_ratios=(0.5, 4),
+    )
+    for _ in range(10):
+        out, new_gsd = t(data, gsd)
+        assert out.shape == (1, 128, 256)
+        assert 0.5 <= new_gsd <= 4.0
+        assert len(torch.unique(out)) == 1
+        assert math.isclose(out[0, 0, 0].item(), new_gsd)
