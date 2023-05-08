8a4a6be11bea0e9520f00bbb753e12bd3df53205
Pierre-Luc St-Charles <pierreluc.stcharles@gmail.com>
2023-05-08 11:38:41 -0400

    Add center fixed crop to geo transforms

--- ssl4rs/data/transforms/geo/crop.py
+++ ssl4rs/data/transforms/geo/crop.py
@@ -1,3 +1,4 @@
+import math
 import typing
 
 import torch
@@ -5,28 +6,32 @@ import torchvision.transforms
 import torchvision.transforms.functional
 from torchvision.transforms import InterpolationMode
 
+import ssl4rs.utils.imgproc
+
 if typing.TYPE_CHECKING:
     from ssl4rs.data import BatchDictType
 
 
 class GroundSamplingDistanceAwareRandomResizedCrop(torch.nn.Module):
-    """Implements a crop function that is aware of a geospatial array's ground sample distance.
+    """Implements a random resized crop function that is aware of the ground sample distance.
 
-    This function works a bit like a random cropper, but it will consider a min/max GSD interval
-    instead of a min/max scale factor to identify the size of the input region to crop and resize.
-    The GSD info must be provided in the batch dictionary or as a call argument at runtime.
+    This "random resized crop" function will output crops with a constant shape. This shape is
+    specified inside the constructor (`size`), but it will contain data from a region in the
+    original array that might be larger or smaller than `size`, and that will have been resized.
+    The target crop area inside the original array is the "random" part of this function: it will
+    be sampled uniformly using the min/max GSD ratios (`gsd_ratios`) specified in the constructor,
+    and applied to the real GSD value found at runtime. This real GSD value must be provided in the
+    batch dictionary or as a call argument at runtime.
 
     The input array is assumed to have a [..., H, W] shape, where the leading dimensions will be
-    left untouched, and where the last two dimensions are expected to be the array height / width.
-    The crop area will be determined based on these two dimensions and on the GSD, and the result
-    will be resized to the target shape.
+    left untouched, and where the last two dimensions are expected to be the array's height and
+    width. The crop area will be determined based on these two dimensions and on the GSD, and the
+    result will be resized to the target shape using torchvision's resize function.
 
     Note that like most torchvision transforms, this class inherits from `torch.nn.Module` in order
     to be compatible with torchscript (and to be compatible with accelerated transform pipelines).
 
     For more info on the arguments, see the `torchvision.transforms.RandomResizedCrop` class.
-
-    TODO @@@@: validate that this is compilable w/ torchscript?
     """
 
     def __init__(
@@ -153,7 +158,7 @@ class GroundSamplingDistanceAwareRandomResizedCrop(torch.nn.Module):
         Returns:
             A tuple of parameters (top-idx, left-idx, height, width) to be passed to a crop
             function to return a crop, and the ground sampling distance (GSD) of the resulting
-            crop.
+            crop (after its future resizing).
         """
         assert input_array.ndim >= 2, "invalid input tensor ndim count"
         input_height, input_width = input_array.shape[-2:]
@@ -258,6 +263,283 @@ class GroundSamplingDistanceAwareRandomResizedCrop(torch.nn.Module):
         return out_str
 
 
+class GroundSamplingDistanceAwareCenterFixedCrop(torch.nn.Module):
+    """Implements a center crop function that is aware of the ground sample distance.
+
+    This "center fixed crop" function will output crops with a constant shape. This shape is
+    specified inside the constructor (`size`), but it will contain data from a region in the
+    original array that might be larger or smaller than `size`, and that will have been resized.
+    The target crop area inside the original array is determined using the `output_gsd` value
+    provided to the constructor. If the original GSD is larger or smaller than the target GSD, the
+    crop area will be adjusted in consequence in order to always provide output crops with the
+    requested shape and GSD values.
+
+    The input array is assumed to have a [..., H, W] shape, where the leading dimensions will be
+    left untouched, and where the last two dimensions are expected to be the array's height and
+    width. The crop area will be determined based on these two dimensions and on the GSD, and the
+    result will be resized to the target shape using torchvision's resize function (if needed).
+
+    Note that like most torchvision transforms, this class inherits from `torch.nn.Module` in order
+    to be compatible with torchscript (and to be compatible with accelerated transform pipelines).
+    """
+
+    def __init__(
+        self,
+        size: typing.Tuple[int, int],
+        output_gsd: typing.Optional[float] = None,  # None = do not resize
+        target_key: typing.Optional[typing.AnyStr] = None,
+        gsd_key: typing.Optional[typing.AnyStr] = None,
+        allow_auto_padding: bool = False,
+        interpolation: InterpolationMode = InterpolationMode.BILINEAR,
+        antialias: bool = True,
+    ):
+        """Validates cropping settings.
+
+        Args:
+            size: expected output size of the crop, for each edge, after resizing. This is expected
+                to be height first, width second, and in pixel values.
+            output_gsd: expected output GSD value for the generated crops. The area in the provided
+                arrays that corresponds to the output crop will vary based on this setting; a
+                larger GSD can be obtained by upsampling a smaller area in the original array, and
+                vice-versa. For example, with an original GSD of 2m and a target output GSD of
+                4m, given a target output size of 256x256 pixels, we will fetch a 512x512 area in
+                the original array, downsample it to 256x256 pixels, and return it. If this value
+                is `None`, this function should be equivalent to a classic `CenterCrop` function.
+            target_key: the key for the target tensor to be cropped, in case we process batch
+                dictionaries. Note that we will replace the tensor by its cropped version in-place
+                within the existing dictionary.
+            gsd_key: the key for the ground sampling distance value to be fetched, in case we
+                process batch dictionaries.
+            allow_auto_padding: defines to auto-pad input arrays when they are too small for the
+                specified crop size/GSD values.
+            interpolation: Desired interpolation enum defined by
+                `torchvision.transforms.InterpolationMode`. Default is bilinear.
+            antialias: defines whether to apply antialiasing. It only affects tensors when the
+                bilinear or bicubic interpolation modes are selected, and is ignored otherwise.
+        """
+        super().__init__()
+        assert len(size) == 2 and all(
+            [isinstance(i, int) and i > 0 for i in size]
+        ), f"invalid size tuple, should be (height, width) in pixels, got: {size}"
+        assert output_gsd is None or output_gsd > 0, f"invalid output gsd: {output_gsd}"
+        if isinstance(interpolation, int):
+            interpolation = torchvision.transforms.functional._interpolation_modes_from_int(interpolation)
+        assert interpolation in InterpolationMode, f"invalid interp mode: {interpolation}"
+        self.size = size
+        self.output_gsd = float(output_gsd) if output_gsd is not None else None
+        self.target_key = target_key
+        self.gsd_key = gsd_key
+        self.allow_auto_padding = allow_auto_padding
+        self.interpolation = interpolation
+        self.antialias = antialias
+
+    def forward(
+        self,
+        data: typing.Union[torch.Tensor, "BatchDictType"],
+        gsd: typing.Optional[float] = None,
+    ) -> typing.Union[typing.Tuple[torch.Tensor, float], "BatchDictType"]:
+        """Crops the specified data array in the provided batch dictionary.
+
+        Note: if the crop size/gsd combos require an area larger than the input array, padding will
+        be added to that array automatically, but only if `allow_auto_padding=True`.
+
+        Args:
+            data: the loaded batch dictionary that contains an array to be cropped, or the tensor
+                to be processed directly. If a batch dictionary is used, it will be UPDATED IN
+                PLACE since we do not deep copy this reference.
+            gsd: the current ground sample distance associated with the array to be cropped. If it
+                is not directly specified via this argument, the `gsd_key` attribute will be used
+                to fetch it from inside the batch dictionary.
+
+        Returns:
+            The cropped tensor and its GSD value, if only a tensor was provided, or the reference
+            to the same (but updated) batch dictionary that contains the crop + GSD value.
+        """
+        if isinstance(data, dict):
+            assert self.target_key is not None, "missing 'target_key' attribute for batch dicts!"
+            input_array = data[self.target_key]
+        else:
+            input_array = data
+        if isinstance(data, torch.Tensor):
+            input_array = data
+        assert isinstance(input_array, torch.Tensor), f"invalid input data type: {type(data)}"
+        assert input_array.ndim >= 2, f"invalid input array ndim: {input_array.ndim}"
+        if gsd is None:
+            assert isinstance(data, dict), "must provide gsd as arg or provide it through batch dict!"
+            assert self.gsd_key is not None, "missing 'gsd_key' attribute for batch dicts!"
+            gsd = data[self.gsd_key]
+        assert isinstance(gsd, float), f"invalid input gsd type: {type(gsd)}"
+        (crop_top_idx, crop_left_idx, crop_height, crop_width, output_gsd) = self.get_params(
+            input_array=input_array,
+            gsd=gsd,
+            size=self.size,
+            output_gsd=self.output_gsd,
+            allow_auto_padding=self.allow_auto_padding,
+        )
+        center_crop = self.get_center_crop(
+            input_array=input_array,
+            crop_top_idx=crop_top_idx,
+            crop_left_idx=crop_left_idx,
+            crop_height=crop_height,
+            crop_width=crop_width,
+            output_height=self.size[0],
+            output_width=self.size[1],
+            allow_auto_padding=self.allow_auto_padding,
+            interpolation=self.interpolation,
+            antialias=self.antialias,
+        )
+        if isinstance(data, dict):
+            data[self.target_key] = center_crop
+            data[self.gsd_key] = output_gsd
+            return data
+        else:
+            return center_crop, output_gsd
+
+    @staticmethod
+    def get_params(
+        input_array: torch.Tensor,
+        gsd: float,
+        size: typing.Tuple[int, int],
+        output_gsd: typing.Optional[float],
+        allow_auto_padding: bool,
+    ) -> typing.Tuple[int, int, int, int, float]:
+        """Returns the parameters for a crop-and-resize operation on the input array.
+
+        Args:
+            input_array: the input array from which a crop should be generated.
+            gsd: the ground sample distance associated with the above array.
+            size: expected output size of the crop, for each edge, after resizing. This is expected
+                to be height first, width second, and in pixel values.
+            output_gsd: expected output GSD value post-resizing. If `None`, no resizing should
+                occur, and the `size` corresponds to the crop area in the original array directly.
+            allow_auto_padding: defines to auto-pad input arrays when they are too small for the
+                specified crop size/GSD values.
+
+        Returns:
+            A tuple of parameters (top-idx, left-idx, height, width) to be passed to a crop
+            function to return a crop, and the ground sampling distance (GSD) of the resulting
+            crop (after resizing, if necessary).
+        """
+        assert input_array.ndim >= 2, "invalid input tensor ndim count"
+        input_height, input_width = input_array.shape[-2:]
+        assert input_height * input_width > 0, "invalid input tensor height/width dims"
+
+        if output_gsd is None or math.isclose(gsd, output_gsd):  # (no resizing)
+            if not allow_auto_padding and (size[0] > input_height or size[1] > input_width):
+                raise ValueError(
+                    "crop size too large for input array!\n"
+                    f"\tcrop size = {size}, input array = {(input_height, input_width)}"
+                )
+            crop_height, crop_width = size[0], size[1]
+            output_gsd = gsd
+        else:
+            # in order to check if we can match the requested GSD, we need to check the max crop's GSD
+            scale_max_h, scale_max_w = input_height / size[0], input_width / size[1]
+            scale_max = min(scale_max_h, scale_max_w)
+            max_possible_gsd = scale_max * gsd
+            if max_possible_gsd < output_gsd and not allow_auto_padding:
+                raise ValueError(
+                    f"cannot create a random crop with output gsd: {output_gsd}\n"
+                    f"\tinput array = ({input_height} x {input_width}) @ {gsd} meters\n"
+                    f"\tresizing a ({int(round(size[0] * scale_max))} x {int(round(size[1] * scale_max))})"
+                    f" crop to ({size[0]} x {size[1]})"
+                    f" would provide a GSD of {max_possible_gsd} meters\n"
+                    f"\t...that would be less than the requested GSD: {output_gsd} meters"
+                )
+            # compute the scale factor & size used for the interpolation of the crop
+            scale = output_gsd / gsd
+            crop_aspect_ratio = size[0] / size[1]
+            crop_height = int(round(size[0] * scale))
+            crop_width = int(round(crop_height / crop_aspect_ratio))
+
+        crop_top = int(round((input_height - crop_height) / 2.0))
+        crop_left = int(round((input_width - crop_width) / 2.0))
+        # note: GSD might be imperfect if we do scaling w/ small crops
+        return crop_top, crop_left, crop_height, crop_width, output_gsd
+
+    @staticmethod
+    def get_center_crop(
+        input_array: torch.Tensor,
+        crop_top_idx: int,
+        crop_left_idx: int,
+        crop_height: int,
+        crop_width: int,
+        output_height: int,
+        output_width: int,
+        allow_auto_padding: bool,
+        interpolation: InterpolationMode,
+        antialias: bool,
+    ) -> torch.Tensor:
+        """Crops the given input array with the specified parameters.
+
+        This function simply calls torchvision's `crop` and `resize` functions under the hood, but
+        may be overridden in derived classes.
+
+        Args:
+            input_array: the input array from which a crop should be generated.
+            crop_top_idx: the index of the row that matches the crop's top left corner.
+            crop_left_idx: the index of the column that matches the crop's top left corner.
+            crop_height: the total height of the crop in the input array (in pixels).
+            crop_width: the total width of the crop in the input array (in pixels).
+            output_height: the expected height of the output (post-resize) array (in pixels).
+            output_width: the expected width of the output (post-resize) array (in pixels).
+            allow_auto_padding: defines to auto-pad input arrays when they are too small for the
+                specified crop size/GSD values.
+            interpolation: Desired interpolation enum defined by
+                `torchvision.transforms.InterpolationMode`. Default is bilinear.
+            antialias: defines whether to apply antialiasing. It only affects tensors when the
+                bilinear or bicubic interpolation modes are selected, and is ignored otherwise.
+
+        Returns:
+            The tensor that corresponds to the specified cropped and resized (with a predetermined
+            interpolation approach) area of the input array.
+        """
+        crop = None
+        if allow_auto_padding:
+            # note: this will likely break torchscript compilations!
+            crop_region = ssl4rs.utils.PatchCoord(
+                top_left=(crop_top_idx, crop_left_idx),
+                shape=(crop_height, crop_width),
+            )
+            image_region = ssl4rs.utils.PatchCoord(
+                top_left=(0, 0),
+                shape=(input_array.shape[-2], input_array.shape[-1]),
+            )
+            if crop_region not in image_region:
+                crop = ssl4rs.utils.imgproc.flex_crop(
+                    image=input_array,
+                    patch=crop_region,
+                )
+        if crop is None:
+            crop = torchvision.transforms.functional.crop(
+                img=input_array,
+                top=crop_top_idx,
+                left=crop_left_idx,
+                height=crop_height,
+                width=crop_width,
+            )
+        assert crop.shape[-2] == crop_height and crop.shape[-1] == crop_width
+        if crop_height != output_height or crop_width != output_width:
+            crop = torchvision.transforms.functional.resize(
+                img=crop,
+                size=(output_height, output_width),  # noqa
+                interpolation=interpolation,
+                antialias=antialias,
+            )
+        return crop
+
+    def __repr__(self) -> str:
+        out_str = self.__class__.__name__ + "("
+        out_str += f"size={self.size}"
+        out_str += f", output_gsd={self.output_gsd}"
+        out_str += f", target_key={self.target_key}"
+        out_str += f", gsd_key={self.gsd_key}"
+        out_str += f", allow_auto_padding={self.allow_auto_padding}"
+        out_str += f", interpolation={self.interpolation}"
+        out_str += f", antialias={self.antialias}"
+        out_str += ")"
+        return out_str
+
+
 # @@@@ TODO: GroundSamplingDistanceAwareJPEGDecoderWithRandomResizedCrop
-# @@@@ TODO: GroundSamplingDistanceAwareCenterResizedCrop
 # @@@@ TODO: GroundSamplingDistanceAwareJPEGDecoderWithCenterResizedCrop
--- tests/data/transforms/geo/crop.py
+++ tests/data/transforms/geo/crop.py
@@ -8,7 +8,7 @@ import torch
 import ssl4rs.data.transforms.geo.crop as geo_crop
 
 
-def test_geo_crop():
+def test_geo_random_resized_crop():
     data = torch.randn(1, 1024, 2048)
     gsd = 0.1
 
@@ -39,7 +39,7 @@ def test_geo_crop():
         assert 0.05 <= updated_dict["carrot"] <= 0.4
 
 
-def test_geo_crop_with_bad_ratio():
+def test_geo_random_resized_crop_with_bad_ratio():
     data = torch.randn(1, 512, 1024)
     gsd = 0.1
     t_ok = geo_crop.GroundSamplingDistanceAwareRandomResizedCrop(
@@ -57,7 +57,7 @@ def test_geo_crop_with_bad_ratio():
         t_bad(data, gsd)
 
 
-def test_geo_crop_with_torchscript():
+def test_geo_random_resized_crop_with_torchscript():
     data = torch.randn(1, 1024, 1024)
     gsd = 0.1
     t = geo_crop.GroundSamplingDistanceAwareRandomResizedCrop(
@@ -81,7 +81,7 @@ def fake_resize(img: torch.Tensor, size: typing.Sequence[int], *args, **kwargs):
 
 
 @mock.patch("torchvision.transforms.functional.resize")
-def test_geo_crop_interpolation(resize_mock):
+def test_geo_random_resized_crop_interpolation(resize_mock):
     # note: this assumes that the base impl relies on torchvision's resize op
     resize_mock.side_effect = fake_resize
     data = torch.ones(1, 1024, 512)
@@ -96,3 +96,124 @@ def test_geo_crop_interpolation(resize_mock):
         assert 0.5 <= new_gsd <= 4.0
         assert len(torch.unique(out)) == 1
         assert math.isclose(out[0, 0, 0].item(), new_gsd)
+
+
+def test_geo_center_fixed_crop():
+    data = torch.randn(1, 1024, 2048)
+    t_without_resize = geo_crop.GroundSamplingDistanceAwareCenterFixedCrop(
+        size=(256, 128),
+        output_gsd=None,
+    )
+    t_with_resize = geo_crop.GroundSamplingDistanceAwareCenterFixedCrop(
+        size=(120, 150),
+        output_gsd=0.5,
+        target_key="potato",
+        gsd_key="carrot",
+    )
+    for _ in range(100):
+        out, new_gsd = t_without_resize(data, 0.1)
+        assert out.shape == (1, 256, 128)
+        assert new_gsd == 0.1
+        out, new_gsd = t_with_resize(data, 0.1)
+        assert out.shape == (1, 120, 150)
+        assert new_gsd == 0.5
+        updated_dict = t_with_resize(
+            {
+                "potato": data,
+                "carrot": 0.1,
+            }
+        )
+        assert not torch.equal(updated_dict["potato"], data)
+        assert updated_dict["potato"].shape == (1, 120, 150)
+        assert updated_dict["carrot"] == 0.5
+
+
+def test_geo_center_fixed_crop_with_bad_crops():
+    data = torch.randn(1, 512, 1024)
+    gsd = 0.1
+    t_ok = geo_crop.GroundSamplingDistanceAwareCenterFixedCrop(
+        size=(256, 500),
+        output_gsd=0.2,
+    )
+    out, new_gsd = t_ok(data, gsd)
+    assert out.shape == (1, 256, 500)
+    assert math.isclose(new_gsd, 0.2)
+    t_ok = geo_crop.GroundSamplingDistanceAwareCenterFixedCrop(
+        size=(512, 1024),
+        output_gsd=None,
+    )
+    out, new_gsd = t_ok(data, gsd)
+    assert out.shape == (1, 512, 1024)
+    assert new_gsd == 0.1
+    t_bad = geo_crop.GroundSamplingDistanceAwareCenterFixedCrop(
+        size=(256, 500),
+        output_gsd=0.21,
+    )
+    with pytest.raises(ValueError):
+        t_bad(data, gsd)
+    t_bad = geo_crop.GroundSamplingDistanceAwareCenterFixedCrop(
+        size=(256, 1200),
+        output_gsd=None,
+    )
+    with pytest.raises(ValueError):
+        t_bad(data, gsd)
+
+
+def test_geo_center_fixed_crop_with_padded_crops():
+    data = torch.ones(1, 512, 1024)
+    gsd = 0.1
+    t_bad = geo_crop.GroundSamplingDistanceAwareCenterFixedCrop(
+        size=(600, 1024),
+        output_gsd=0.1,
+    )
+    with pytest.raises(ValueError):
+        t_bad(data, gsd)
+    t_ok = geo_crop.GroundSamplingDistanceAwareCenterFixedCrop(
+        size=(600, 1024),
+        output_gsd=0.1,
+        allow_auto_padding=True,
+    )
+    out, new_gsd = t_ok(data, gsd)
+    assert out.shape == (1, 600, 1024)
+    assert new_gsd == 0.1
+    assert torch.count_nonzero(out == 0).item() == (600 - 512) * 1024
+
+
+def test_geo_center_fixed_crop_with_torchscript():
+    data = torch.randn(1, 1024, 1024)
+    gsd = 0.1
+    t = geo_crop.GroundSamplingDistanceAwareCenterFixedCrop(
+        size=(256, 256),
+        output_gsd=0.2,
+    )
+    t_compiled = torch.compile(t)
+    for _ in range(10):
+        out, new_gsd = t_compiled(data, gsd)
+        assert out.shape == (1, 256, 256)
+        assert new_gsd == 0.2
+
+
+@mock.patch("torchvision.transforms.functional.resize")
+def test_geo_center_fixed_crop_interpolation(resize_mock):
+    # note: this assumes that the base impl relies on torchvision's resize op
+    resize_mock.side_effect = fake_resize
+    data = torch.ones(1, 1024, 512)
+    gsd = 1.0
+    t_downsample = geo_crop.GroundSamplingDistanceAwareCenterFixedCrop(
+        size=(128, 256),
+        output_gsd=2.0,
+    )
+    out, new_gsd = t_downsample(data, gsd)
+    assert out.shape == (1, 128, 256)
+    assert new_gsd == 2.0
+    assert len(torch.unique(out)) == 1
+    assert math.isclose(out[0, 0, 0].item(), new_gsd)
+    t_upsample = geo_crop.GroundSamplingDistanceAwareCenterFixedCrop(
+        size=(256, 128),
+        output_gsd=0.5,
+    )
+    out, new_gsd = t_upsample(data, gsd)
+    assert out.shape == (1, 256, 128)
+    assert new_gsd == 0.5
+    assert len(torch.unique(out)) == 1
+    assert math.isclose(out[0, 0, 0].item(), new_gsd)
