dd33609bc0be59e9867a93db396b7559fc233a92
Pierre-Luc St-Charles <pierreluc.stcharles@gmail.com>
2024-03-29 16:01:39 -0400

    Add dropout+norm to simple mlp impl

--- ssl4rs/models/components/simple_mlp.py
+++ ssl4rs/models/components/simple_mlp.py
@@ -16,6 +16,8 @@ class SimpleMLP(torch.nn.Module):
         in_channels: int,
         hidden_channels: typing.Sequence[int],
         out_channels: int,
+        with_batch_norm: bool = False,
+        dropout: float = 0.0,
     ):
         """Initializes the MLP using the provided settings."""
         assert isinstance(in_channels, int) and in_channels > 0
@@ -29,11 +31,15 @@ class SimpleMLP(torch.nn.Module):
             build_layer(
                 in_channels=in_channels,
                 out_channels=hidden_channels[0],
+                with_batch_norm=with_batch_norm,
+                dropout=dropout,
             ),
             *[
                 build_layer(
                     in_channels=hidden_channels[idx],
                     out_channels=hidden_channels[idx + 1],
+                    with_batch_norm=with_batch_norm,
+                    dropout=dropout,
                 )
                 for idx in range(0, len(hidden_channels) - 1)
             ],
@@ -55,9 +61,11 @@ class SimpleMLP(torch.nn.Module):
 def build_layer(
     in_channels: int,
     out_channels: int,
-    with_batch_norm: bool = True,
+    with_batch_norm: bool = False,
+    dropout: float = 0.0,
 ) -> torch.nn.Module:
     """Returns a Conv2d-BN-ReLU layer with optional pooling based on PyTorch modules."""
+    assert 0 <= dropout <= 1.0
     modules = [
         torch.nn.Linear(
             in_features=in_channels,
@@ -65,5 +73,6 @@ def build_layer(
         ),
         torch.nn.BatchNorm1d(out_channels) if with_batch_norm else torch.nn.Identity(),
         torch.nn.ReLU(),
+        torch.nn.Dropout(p=dropout) if dropout > 0 else torch.nn.Identity(),
     ]
     return torch.nn.Sequential(*modules)
