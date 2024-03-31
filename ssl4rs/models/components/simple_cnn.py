8f3e7f7f27a9bff8645ca5bc5fa1e79a5f34745b
Pierre-Luc St-Charles <pierreluc.stcharles@gmail.com>
2024-03-30 23:27:28 -0400

    Update simplecnn impl w/ dropout, upsampler

--- ssl4rs/models/components/simple_cnn.py
+++ ssl4rs/models/components/simple_cnn.py
@@ -7,6 +7,7 @@ import torch.nn
 default_kernel_size = (3, 3)
 default_stride = (1, 1)
 default_padding = (1, 1)
+default_dropout = 0.0
 
 
 class SimpleConvNet(torch.nn.Module):
@@ -19,16 +20,21 @@ class SimpleConvNet(torch.nn.Module):
         self,
         in_channels: int,
         hidden_channels: typing.Sequence[int],
-        head_channels: typing.Optional[int] = None,  # for a final 1x1 conv head, if needed
         kernel_sizes: typing.Optional[typing.Sequence[typing.Tuple[int, int]]] = None,
         strides: typing.Optional[typing.Sequence[typing.Tuple[int, int]]] = None,
         paddings: typing.Optional[typing.Sequence[typing.Tuple[int, int]]] = None,
+        dropouts: typing.Optional[typing.Sequence[float]] = None,
+        with_batch_norm: bool = True,
+        with_max_pool: bool = True,
+        with_output_upsample: bool = False,
+        head_channels: typing.Optional[int] = None,  # for a final 1x1 conv head, if needed
     ):
         """Initializes the CNN using the provided settings."""
         assert len(hidden_channels) > 0 and all([c > 0 for c in hidden_channels])
         assert kernel_sizes is None or len(kernel_sizes) == len(hidden_channels)
         assert strides is None or len(strides) == len(hidden_channels)
         assert paddings is None or len(paddings) == len(hidden_channels)
+        assert dropouts is None or len(dropouts) == len(hidden_channels)
         super().__init__()
         self.model = torch.nn.Sequential(
             build_layer(
@@ -37,6 +43,9 @@ class SimpleConvNet(torch.nn.Module):
                 kernel_size=kernel_sizes[0] if kernel_sizes else default_kernel_size,
                 stride=strides[0] if strides else default_stride,
                 padding=paddings[0] if paddings else default_padding,
+                dropout=dropouts[0] if dropouts else default_dropout,
+                with_batch_norm=with_batch_norm,
+                with_max_pool=with_max_pool,
             ),
             *[
                 build_layer(
@@ -45,6 +54,9 @@ class SimpleConvNet(torch.nn.Module):
                     kernel_size=kernel_sizes[idx + 1] if kernel_sizes else default_kernel_size,
                     stride=strides[idx + 1] if strides else default_stride,
                     padding=paddings[idx + 1] if paddings else default_padding,
+                    dropout=dropouts[idx + 1] if dropouts else default_dropout,
+                    with_batch_norm=with_batch_norm,
+                    with_max_pool=with_max_pool,
                 )
                 for idx in range(0, len(hidden_channels) - 1)
             ],
@@ -54,6 +66,13 @@ class SimpleConvNet(torch.nn.Module):
                 kernel_size=(1, 1),
             )
             if head_channels is not None
+            else torch.nn.Identity(),
+            torch.nn.Upsample(
+                scale_factor=2,
+                mode="bilinear",
+                align_corners=True,
+            )
+            if with_output_upsample is not None
             else torch.nn.Identity(),
         )
 
@@ -70,6 +89,7 @@ def build_layer(
     kernel_size: typing.Tuple[int, int] = (3, 3),
     stride: typing.Tuple[int, int] = (1, 1),
     padding: typing.Tuple[int, int] = (1, 1),
+    dropout: float = 0.0,
     with_batch_norm: bool = True,
     with_max_pool: bool = True,
 ) -> torch.nn.Module:
@@ -84,6 +104,7 @@ def build_layer(
         ),
         torch.nn.BatchNorm2d(out_channels) if with_batch_norm else torch.nn.Identity(),
         torch.nn.ReLU(),
+        torch.nn.Dropout(p=dropout) if dropout > 0 else torch.nn.Identity(),
     ]
     if with_max_pool:
         modules.append(torch.nn.MaxPool2d(kernel_size=(2, 2)))
